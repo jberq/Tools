b3568fde0717eaec13f5f2eb02575420b8ddd9e3
jberq <jberq@live.com>
2021-07-15 13:19:45 -0500

    Chocolately stuff

--- Powershell/Get-ChocoList.ps1
+++ Powershell/Get-ChocoList.ps1
@@ -1,9 +1,16 @@
 function Get-ChocoList {
-	(choco list -l -r).foreach{
-		$app = $_.split('|')
-		[PSCustomObject]@{
-			Name = $app[0]
-			CurrentVersion = $app[1]
-		}
-	}
+    try {
+        if (Get-Command -CommandType Application -Name choco) {
+            (choco list -l -r).foreach{
+                $app = $_.split('|')
+                [PSCustomObject]@{
+                    Name           = $app[0]
+                    CurrentVersion = $app[1]
+                }
+            }
+        }
+    }
+    catch {
+        Write-Error "choco not found in PATH."
+    }
 }
--- Powershell/Get-ChocoOutdated.ps1
+++ Powershell/Get-ChocoOutdated.ps1
@@ -1,10 +1,17 @@
 function Get-ChocoOutdated {
-	(choco outdated -r).foreach{
-		$app = $_.split('|')
-		[PSCustomObject]@{
-			Name = $app[0]
-			CurrentVersion = $app[1]
-			AvailableVersion = $app[2]
-		}
-	}
+    try {
+        if (Get-Command -CommandType Application -Name choco) {
+            (choco outdated -r).foreach{
+                $app = $_.split('|')
+                [PSCustomObject]@{
+                    Name = $app[0]
+                    CurrentVersion = $app[1]
+                    AvailableVersion = $app[2]
+                }
+            }
+        }
+    }
+    catch {
+        Write-Error "choco not found in PATH."
+    }
 }
